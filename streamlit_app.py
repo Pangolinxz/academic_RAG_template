99db27a92a9dcfe36a0476fe94ef012db2cca78a
jmejiaes <juanmejiadev@gmail.com>
2024-11-27 09:07:46 -0500

    templater for streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,11 @@ assistant = AcademicAssistant()
 
 # obtener respuesta del chatbot
 def get_bot_response(user_input, mode):
-    prompt_template = assistant.select_prompt_template(mode)
-    response_content = assistant.generate_response(user_input, prompt_template)
+    prompt_template = assistant.__________(mode)
+    response_content = assistant._________(user_input, prompt_template)
     return response_content
+
+
 
 # Simulación de respuesta del bot palabra por palabra
 def response_generator(message: str):
@@ -22,19 +24,28 @@ def response_generator(message: str):
         time_to_sleep = random.random() * 0.1
         time.sleep(time_to_sleep)
 
+
+
+
 # Configuración inicial del estado
 if "mode" not in st.session_state:
     st.session_state.mode = "Repaso 🧠"  # Modo predeterminado
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+
+
+
 # Ruta del avatar del usuario
 USER_AVATAR = "assets/student_logo.svg"
 BOT_AVATAR = "assets/assistant_logo.svg"
 
 
+
+
+
 # Barra lateral para selección del modo (centrado)
-st.sidebar.markdown(
+st.sidebar.______(
     """
     <div style="text-align: center;">
         <h2>Modo de Estudio</h2>
@@ -43,8 +54,12 @@ st.sidebar.markdown(
     unsafe_allow_html=True
 )
 
+
+
+
+
 # Radio buttons para seleccionar el modo
-mode = st.sidebar.radio(
+mode = st._______.______(
     "Seleccione el modo de estudio",
     ["Repaso 🧠", "Simulacro 📝"],
     label_visibility="collapsed",
@@ -53,20 +68,24 @@ mode = st.sidebar.radio(
 st.session_state.mode = mode
 
 # Espaciador para centrar el botón de borrar chat
-st.sidebar.markdown("<br><br>"*10, unsafe_allow_html=True)
+st._____.______(
+    "<br><br>"*10, 
+    unsafe_allow_html=True
+    )
 
-# Centrar el botón utilizando columnas
-col1, col2, col3 = st.sidebar.columns([1, 2, 1])  
-with col2:
-    if st.button("🗑️ Borrar chat", key="clear_chat"):
-        st.session_state.messages = []
-        st.sidebar.success("¡El historial del chat ha sido borrado!")
+
+# Botón de borrado del chat
+if st.______("🗑️ Borrar chat", key="clear_chat"):
+    st.______.______ = []
+    st.sidebar.______("¡El historial del chat ha sido borrado!")
+
+
 
 # Pantalla principal del chatbot
 
 # Título centrado
-st.markdown(
-    "<h1 style='text-align: center;'>Asistente Académico</h1>",
+st.______(
+    "<h1 style='text-align: center;'>_________________</h1>",
     unsafe_allow_html=True
 )
 
@@ -77,34 +96,37 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+
+
+
 # Mostrar mensajes del historial
-for message in st.session_state.messages:
-    with st.chat_message(message["role"], avatar=USER_AVATAR if message["role"] == "user" else BOT_AVATAR):
-        st.markdown(message["content"])
+for message in st.___________.____________:
+    with st.___________(message["role"], avatar=USER_AVATAR if message["role"] == "user" else BOT_AVATAR):
+        st.____________(message["content"])
 
 # Campo de entrada del usuario
-if prompt := st.chat_input("Escribe tu mensaje:"):
+if prompt := st.______________("Escribe tu mensaje:"):
 
     # Agregar mensaje del usuario al historial
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.___________.___________({"role": "user", "content": prompt})
 
     # Mostrar el mensaje del usuario con su avatar
-    with st.chat_message("user", avatar=USER_AVATAR):
-        st.markdown(prompt)
+    with st.___________("user", avatar=USER_AVATAR):
+        st.____________(prompt)
 
     # Obtener respuesta del bot
     try:
-        bot_response = get_bot_response(prompt, st.session_state.mode[:-2].lower())
+        bot_response = ______________( _____________ , _____________[:-2].lower())
 
     except:
         bot_response = "No se pudo conectar con el servidor."
 
     # Mostrar respuesta del bot con animación
-    with st.chat_message("assistant", avatar=BOT_AVATAR):
-        st.write_stream(response_generator(bot_response))
+    with st.____________( "assistant", avatar=BOT_AVATAR ):
+        st._____________( response_generator(bot_response) )
 
     # Agregar respuesta al historial
-    st.session_state.messages.append({"role": "assistant", "content": bot_response})
+    st.___________.___________._________({"role": "assistant", "content": bot_response})
 
 
 
